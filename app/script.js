5946770435f3070e21fe6536c319a304598a70c4
charbel-dot <nassehcharbel@gmail.com>
2023-06-19 18:57:07 +0300

    Added geolocation code

--- app/script.js
+++ app/script.js
@@ -73,3 +73,24 @@ const displayTime = () => {
 
 // Update time
 setInterval(displayTime, updateTime);
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+const success = (pos) => {
+  const crd = pos.coords;
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+};
+
+const error = (err) => {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
